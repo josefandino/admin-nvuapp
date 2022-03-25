d0bdb0ff6b28946e42f9a9448034e4ef227efc46
josefandinod <josefandinod@gmail.com>
2022-03-25 08:30:40 -0500

    agregar Facultad

--- src/app/models/panel/facultad.ts
+++ src/app/models/panel/facultad.ts
@@ -5,11 +5,11 @@ export interface FacultadI {
   numeroCiclo:   string;
   teacher: string;
   redirectTo: string;
-  ciclos: string;
   clasificacion: string;
   codeCarrera: string;
   authorCarrera: string;
   note: string;
+  description: string;
   status: boolean;
   dateCteated: string;
 }
---
+++ src/app/services/panel/facultad/facultad.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FacultadService {
+
+  constructor(
+    private facultadFireSvc: AngularFirestore
+  ) { }
+
+  async create(collection, dato) {
+    try {
+      return await this.facultadFireSvc.collection(collection).add(dato);
+    } catch (err) { alert('ERROR en : ' + JSON.stringify(err)); }
+  }
+
+  async getByIdFacultad(collection, id) {
+    try {
+      return await this.facultadFireSvc.collection(collection).doc(id).get();
+    } catch (err) { alert('ERROR en: ' + JSON.stringify(err)); }
+  }
+
+  async getFacultades(collection) {
+    try {
+      return await this.facultadFireSvc.collection(collection).snapshotChanges();
+    } catch (err) { alert('ERROR en: ' + JSON.stringify(err)); }
+  }
+
+  async updateFacultad(collection, id, dato) {
+    try {
+      return await this.facultadFireSvc.collection(collection).doc(id).set(dato);
+    } catch (err) { alert('ERROR en: ' + JSON.stringify(err)); }
+  }
+
+  async deleteFacultad(collection, id) {
+    try {
+      return await this.facultadFireSvc.collection(collection).doc(id).delete();
+    } catch (err) { alert('ERROR en: ' + JSON.stringify(err)); }
+  }
+
+}
